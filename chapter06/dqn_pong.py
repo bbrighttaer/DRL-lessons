358199e898064eaf3d31f607f6a9fc861d386f4c
bbrighttaer <brighteragyemang@gmail.com>
2020-01-24 18:01:55 +0800

    DQN pong

---
+++ chapter06/dqn_pong.py
@@ -0,0 +1,185 @@
+# Author: bbrighttaer
+# Project: DRL-lessons
+# Date: 1/23/20
+# Time: 2:31 PM
+# File: dqn_pong.py
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import collections
+import time
+
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from utils import wrappers as wp
+from utils.dqn_model import DQN
+
+DEFAULT_ENV_NAME = 'PongNoFrameskip-v4'
+MEAN_REWARD_BOUND = 19.5
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+REPLAY_SIZE = 10000
+LEARNING_RATE = 1e-4
+SYNC_TARGET_FRAMES = 1000
+REPLAY_START_SIZE = 10000
+
+EPSILON_DECAY_LAST_FRAME = 10 ** 5
+EPSILON_START = 1.0
+EPSILON_FINAL = 0.02
+
+Experience = collections.namedtuple('Experience', field_names=['state', 'action', 'reward', 'done', 'new_state'])
+
+
+def make_env(env_name):
+    env = gym.make(env_name)
+    env = wp.MaxAndSkipEnv(env)
+    env = wp.FireResetEnv(env)
+    env = wp.ProcessFrame84(env)
+    env = wp.ImageToPyTorch(env)
+    env = wp.BufferWrapper(env, 4)
+    return wp.ScaledFloatFrame(env)
+
+
+class ExperienceBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
+        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
+               np.array(dones, dtype=np.uint8), np.array(next_states)
+
+
+class Agent:
+    def __init__(self, env, exp_buffer):
+        self.env = env
+        self.exp_buffer = exp_buffer
+        self._reset()
+
+    def _reset(self):
+        self.state = self.env.reset()
+        self.total_reward = 0.0
+
+    def play_step(self, net, epislon=0.0, device='cpu'):
+        done_reward = None
+        if np.random.random() < epislon:
+            action = self.env.action_space.sample()
+        else:
+            state_a = np.array([self.state], copy=False)
+            state_v = torch.tensor(state_a).to(device)
+            q_vals_v = net(state_v)
+            _, act_v = torch.max(q_vals_v, dim=1)
+            action = int(act_v.item())
+
+        new_state, reward, is_done, _ = self.env.step(action)
+        self.total_reward += reward
+
+        exp = Experience(self.state, action, reward, is_done, new_state)
+        self.exp_buffer.append(exp)
+        self.state = new_state
+        if is_done:
+            done_reward = self.total_reward
+            self._reset()
+        return done_reward
+
+
+def calc_loss(batch, net, tgt_net, device='cpu'):
+    states, actions, rewards, dones, next_states = batch
+
+    # convert numpy arrays to torch tensors
+    states_v = torch.tensor(states).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.tensor(dones, dtype=torch.bool).to(device)
+
+    state_action_values = net(states_v)
+    state_action_values = state_action_values.gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+    next_state_values = tgt_net(next_states_v).max(1)[0]
+    next_state_values[done_mask] = 0.0
+    next_state_values = next_state_values.detach()
+
+    expected_state_action_values = rewards_v + GAMMA * next_state_values
+    loss = nn.MSELoss()(state_action_values, expected_state_action_values)
+    return loss
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("--env", default=DEFAULT_ENV_NAME,
+                        help="Name of the environment, default=" + DEFAULT_ENV_NAME)
+    parser.add_argument("--reward", type=float, default=MEAN_REWARD_BOUND,
+                        help="Mean reward boundary for stop of training, default=%.2f" % MEAN_REWARD_BOUND)
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    env = make_env(args.env)
+
+    # create policy and target net
+    net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    print(net)
+
+    buffer = ExperienceBuffer(REPLAY_SIZE)
+    agent = Agent(env, buffer)
+    epsilon = EPSILON_START
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+    total_rewards = []
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+    best_mean_reward = None
+
+    while True:
+        frame_idx += 1
+        # epsilon decay strategy
+        epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
+
+        reward = agent.play_step(net, epsilon, device)
+        if reward is not None:
+            total_rewards.append(reward)
+            speed = (frame_idx - ts_frame) / (time.time() - ts)
+            ts_frame = frame_idx
+            ts = time.time()
+            mean_reward = np.mean(total_rewards[-100:])
+            print(f'{frame_idx}: done {len(total_rewards)} games, mean reward {mean_reward:.3f},'
+                  f' eps {epsilon:.2f}, speed {speed:.2f}')
+            if best_mean_reward is None or best_mean_reward < mean_reward:
+                torch.save(net.state_dict(), args.env + '-best.mod')
+                if best_mean_reward is not None:
+                    print(f'Best mean reward updated {best_mean_reward:.3f} -> {mean_reward:.3f}')
+                best_mean_reward = mean_reward
+            if mean_reward > args.reward:
+                print(f'Solved in {frame_idx} frames')
+                break
+
+        if len(buffer) < REPLAY_START_SIZE:
+            continue
+
+        if frame_idx % SYNC_TARGET_FRAMES == 0:
+            tgt_net.load_state_dict(net.state_dict())
+
+        optimizer.zero_grad()
+        batch = buffer.sample(BATCH_SIZE)
+        loss_t = calc_loss(batch, net, tgt_net, device)
+        loss_t.backward()
+        optimizer.step()
